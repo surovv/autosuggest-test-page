fd5ef47e91112d90820ae4a02f52189b3be1690f
Sergey Zhulin <brainsurov@gmail.com>
2019-07-24 18:55:29 +0300

    Add content to App component for better Autosuggest illustration

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,15 @@ import Autosuggest from '../Autosuggest';
 const App = () => (
   <div className={styles.root}>
     <Autosuggest />
+
+    <h1 className={styles.title}>
+      website content
+    </h1>
+    <div className={styles.dot} />
+    <div className={styles.text}>
+      プリンターで作成されたドレスや磁力をデザインに取り入れたドレスなど、
+      メトロポリタン美術館やパリのパレ・ド・トーキョーでも展示が行われた。
+    </div>
   </div>
 );
 
